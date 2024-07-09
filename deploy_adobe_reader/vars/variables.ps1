2b6b0e01dbeae132cd0b1c9015b0397aceee35c5
Ernst Murk <ernstmurk@casema.nl>
2024-07-09 15:15:02 +0200

    reverted changes since it was a user only install

--- deploy_adobe_reader/vars/variables.ps1
+++ deploy_adobe_reader/vars/variables.ps1
@@ -1,15 +1,12 @@
 # Determine the parent directory of the script directory
 $parentDir = Split-Path -Parent (Split-Path -Parent $MyInvocation.MyCommand.Path)
 
-# Determine the current user's username
-$currentUser = $env:USERNAME
-
 # Pad naar het installatie bestand van de applicatie.
 $sourceFolder = "$parentDir\source\Adobe Reader"
 
 # Locatie waar de installatie tijdelijk naar toe wordt gekopieerd op de server zodat het vanaf die locatie gestart kan worden,
 # deze locatie wordt verwijderd op het einde van het script.
-$destinationFolder = "C:\Users\$currentUser\temp"
+$destinationFolder = "C:\temp"
 
 # Lijst van servers waar het script een connectie mee maakt en de software installeert.
 $computers = Get-Content "$parentDir\servers.txt"
